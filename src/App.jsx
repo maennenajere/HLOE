f7684d2cf8b5d0a369bf785f2b3a8026c26332df
Jere Mäennenä <jere.maennena@gmail.com>
2025-03-17 21:37:37 +0200

    update Vercel Speed Insights import and add Next.js dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Analytics } from "@vercel/analytics/react";
 import './index.css';
 
